f84bf4b69ca7b8d971b366d255bffe2c6d283101
EC2 Default User <ec2-user@ip-172-31-12-196.ec2.internal>
2021-11-04 06:07:31 +0000

    [Update]3-5 閲覧機能追加

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,31 @@
+class TodolistsController < ApplicationController
+  def new
+    #Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する
+   @list = List.new
+  end
+
+ def create
+  # 1.データを新規登録するためのインスタンス作成
+  list = List.new(list_params)
+  # 2.データをデータベースに保存するためのsaveメソッド実行
+  list.save
+  # 3.詳細画面へのリダイレクト
+  redirect_to todolist_path(list.id)
+ end
+
+  def index
+    @lists=List.all
+  end
+
+  def show
+    #レコード１件を取得するので単数形
+    @list =List.find(params[:id])
+  end
+
+ private
+ #ストロングパラメータ
+ def list_params
+  params.require(:list).permit(:title,:body)
+ end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   get "top"=> "homes#top"
+  get 'todolists/new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
+  post 'todolists' => 'todolists#create'
+  get "todolists" => "todolists#index"
+  #.../todolists/1や.../todolists/3に該当する
+  get "todolists/:id" => "todolists#show",as:"todolist"
 end
